cfdb5d0ff938d0803ead13b80551d53b5d7c0945
JRempel <Jonathan.Rempel@dal.ca>
2018-03-17 19:12:37 -0300

    Removed extraneous definitions

--- src/jSol/Predict.java
+++ src/jSol/Predict.java
@@ -6,32 +6,32 @@ import static jSol.Term.*;
 import static jSol.Token.*;
 
 public class Predict {
-    private static HashMap<Term, HashMap<Token, Term[]>> productions = new HashMap<Term, HashMap<Token, Term[]>>() {{
-        put(Program, new HashMap<Token, Term[]>() {{
+    private static HashMap<Term, HashMap<Token, Term[]>> productions = new HashMap<>() {{
+        put(Program, new HashMap<>() {{
             put(COFUN, new Term[]{TopDef, Program});
             put(FUN, new Term[]{TopDef, Program});
             put(TYPE, new Term[]{TopDef, Program});
             put(EOP, new Term[]{Epsilon});
         }});
-        put(TopDef, new HashMap<Token, Term[]>() {{
+        put(TopDef, new HashMap<>() {{
             put(COFUN, new Term[]{CoFunDef});
             put(FUN, new Term[]{FunDef});
             put(TYPE, new Term[]{TypeDef});
         }});
-        put(FunDef, new HashMap<Token, Term[]>() {{
+        put(FunDef, new HashMap<>() {{
             put(FUN, new Term[]{KwFun, KwId, Body, KwEnd});
         }});
-        put(CoFunDef, new HashMap<Token, Term[]>() {{
+        put(CoFunDef, new HashMap<>() {{
             put(COFUN, new Term[]{KwCoFun, KwId, Body, KwEnd});
         }});
-        put(TypeDef, new HashMap<Token, Term[]>() {{
+        put(TypeDef, new HashMap<>() {{
             put(TYPE, new Term[]{KwType, KwId, Fields, KwEnd});
         }});
-        put(Fields, new HashMap<Token, Term[]>() {{
+        put(Fields, new HashMap<>() {{
             put(ID, new Term[]{KwId, Fields});
             put(END, new Term[]{Epsilon});
         }});
-        put(Body, new HashMap<Token, Term[]>() {{
+        put(Body, new HashMap<>() {{
             put(FLOAT, new Term[]{Statement, Body});
             put(COFUN, new Term[]{Statement, Body});
             put(REFERENCE, new Term[]{Statement, Body});
@@ -49,7 +49,7 @@ public class Predict {
             put(SYMBOL, new Term[]{Statement, Body});
             put(CFLAMBDAEND, new Term[]{Epsilon});
         }});
-        put(Statement, new HashMap<Token, Term[]>() {{
+        put(Statement, new HashMap<>() {{
             put(FLOAT, new Term[]{SimpleStatement});
             put(COFUN, new Term[]{Def});
             put(REFERENCE, new Term[]{SimpleStatement});
@@ -64,16 +64,16 @@ public class Predict {
             put(CFLAMBDABEGIN, new Term[]{Lambda});
             put(SYMBOL, new Term[]{SimpleStatement});
         }});
-        put(Def, new HashMap<Token, Term[]>() {{
+        put(Def, new HashMap<>() {{
             put(COFUN, new Term[]{TopDef});
             put(FUN, new Term[]{TopDef});
             put(TYPE, new Term[]{TopDef});
             put(VAR, new Term[]{VarDef});
         }});
-        put(VarDef, new HashMap<Token, Term[]>() {{
+        put(VarDef, new HashMap<>() {{
             put(VAR, new Term[]{KwVar, KwId});
         }});
-        put(SimpleStatement, new HashMap<Token, Term[]>() {{
+        put(SimpleStatement, new HashMap<>() {{
             put(FLOAT, new Term[]{Float});
             put(REFERENCE, new Term[]{KwReference});
             put(ID, new Term[]{KwId});
@@ -82,14 +82,14 @@ public class Predict {
             put(STRING, new Term[]{String});
             put(SYMBOL, new Term[]{KwSymbol});
         }});
-        put(Lambda, new HashMap<Token, Term[]>() {{
+        put(Lambda, new HashMap<>() {{
             put(LAMBDABEGIN, new Term[]{FunLambda});
             put(CFLAMBDABEGIN, new Term[]{CoFunLambda});
         }});
-        put(FunLambda, new HashMap<Token, Term[]>() {{
+        put(FunLambda, new HashMap<>() {{
             put(LAMBDABEGIN, new Term[]{KwLambdaBegin, Body, KwLambdaEnd});
         }});
-        put(CoFunLambda, new HashMap<Token, Term[]>() {{
+        put(CoFunLambda, new HashMap<>() {{
             put(CFLAMBDABEGIN, new Term[]{KwCFLambdaBegin, Body, KwCFLambdaEnd});
         }});
     }};
