21b55a4372e4bcb1b004b3ef9fbb12a258677e82
JRempel <Jonathan.Rempel@dal.ca>
2018-03-29 12:45:11 -0300

    Begin AST stuff...

---
+++ src/jSol/AST.java
@@ -0,0 +1,76 @@
+package jSol;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public enum AST {
+    Program, Function, CoRoutine, VarUse, RefUse, Int, Float, Char, String;
+
+    private HashMap<String, int[]> symbols;
+    private List<AST> statements;
+    private String value;
+
+    AST() {
+        symbols = new HashMap<>();
+        statements = new ArrayList<>();
+        value = "";
+    }
+
+    public java.lang.String getValue() {
+        return value;
+    }
+
+    public void setValue(java.lang.String value) {
+        this.value = value;
+    }
+
+    public HashMap<java.lang.String, int[]> getSymbols() {
+        return symbols;
+    }
+
+    public void addSymbol(String symbol, int[] value) {
+        symbols.put(symbol, value);
+    }
+
+    public List<AST> getStatements() {
+        return statements;
+    }
+
+    public void addStatement(AST statement) {
+        statements.add(statement);
+    }
+
+    public void print() {
+        print(0);
+    }
+
+    private void print(int tabs) {
+        switch (this) {
+            case Int:
+            case Char:
+            case Float:
+            case String:
+            case RefUse:
+            case VarUse:
+                System.out.println(Collections.nCopies(tabs, "  ") + this.name() + " = " + value);
+                break;
+            case Program:
+            case Function:
+            case CoRoutine:
+                System.out.println(Collections.nCopies(tabs, "  ") + this.name());
+                System.out.print(Collections.nCopies(tabs + 1, "  ") +"Symbols: ");
+                for (var s : symbols.entrySet()) {
+                    System.out.print(s.getKey() + "(" + Arrays.toString(s.getValue()) + ") ");
+                }
+                System.out.println();
+                System.out.print(Collections.nCopies(tabs + 1, "  ") +"Statements: ");
+                for (var s : statements) {
+                    print(tabs + 2);
+                }
+                break;
+        }
+    }
+}
--- src/jSol/AbstractSyntaxTree.java
+++ src/jSol/AbstractSyntaxTree.java
@@ -1,7 +1,70 @@
 package jSol;
 
+import java.util.Set;
+
+import static jSol.Term.*;
+
 public class AbstractSyntaxTree {
+    private static Set<Term> hasChildren = Set.of(Program, TopDef, FunDef, CoFunDef, TypeDef,
+            Fields, Body, Statement, Def, VarDef, SimpleStatement, Lambda, FunLambda, CoFunLambda);
+
+    private AST root;
+    private Set<String> strings;
+    private Set<String> types;
+
+    public AST getRoot() {
+        return root;
+    }
+
+    public void setRoot(AST root) {
+        this.root = root;
+    }
+
+    public Set<java.lang.String> getStrings() {
+        return strings;
+    }
+
+    public void setStrings(Set<java.lang.String> strings) {
+        this.strings = strings;
+    }
+
+    public Set<java.lang.String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(Set<java.lang.String> types) {
+        this.types = types;
+    }
+
     public static AbstractSyntaxTree fromParseTree(ParseTree tree) {
         return new AbstractSyntaxTree();
+    }
+
+    private static AbstractSyntaxTree toAST(ParseTree tree) {
+        for (var node : tree.getChildren()) {
+            if (hasChildren.contains(node.getType())) {
+                var result = toAST(node);
+                // collect results to AST Obj
+                // If node is Function / Program??
+                // Switch(type)??
+                return new AbstractSyntaxTree();
+            } else {
+                // add self
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("Strings: ");
+        for (var s : strings) {
+            System.out.println("  " + s);
+        }
+        System.out.println("Types: ");
+        for (var type: types) {
+            System.out.print(type);
+        }
+        System.out.println();
+        System.out.println("AST: ");
+        root.print();
     }
 }
